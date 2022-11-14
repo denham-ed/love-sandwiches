54565adf4fb65d9aaf51b1940f12b358d23c7f6e
Ed Denham <ed.denham@gmail.com>
2022-11-14 23:23:41 +0000

    Add user request loop to repeat request if data is invalid

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -21,13 +21,16 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas")
-    print("Example: 10,20,30,40,50,60 \n")
-    data_str = input("Enter your data here:")
-    # print(f"The data provided is {data_str}")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas")
+        print("Example: 10,20,30,40,50,60 \n")
+        data_str = input("Enter your data here:")
+        # print(f"The data provided is {data_str}")
+        sales_data = data_str.split(",")
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
 
 
 def validate_data(values):
@@ -37,14 +40,17 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
-            [int(value) for value in values
-            ]
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
+
     except ValueError as e:
         print(f"Invalid data: {e}, please try again. \n")
+        return False
+
+    return True
 
 
 get_sales_data()
