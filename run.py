8a798abc44da0c5b82922201a65c249cec66f81a
Ed Denham <ed.denham@gmail.com>
2022-11-15 00:24:39 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,10 +13,6 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-# sales = SHEET.worksheet('sales')
-# data = sales.get_all_values()
-# print(data)
-
 
 def get_sales_data():
     """
@@ -26,7 +23,6 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas")
         print("Example: 10,20,30,40,50,60 \n")
         data_str = input("Enter your data here:")
-        # print(f"The data provided is {data_str}")
         sales_data = data_str.split(",")
         if validate_data(sales_data):
             print("Data is valid!") 
@@ -64,8 +60,34 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated successfully. \n")
 
 
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+    The surplus is defined as the sales figure substracted from the stock:
+    - Postive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet('stock').get_all_values()
+    stock_row = stock[-1]
+    surplus_data = []
+    for stock, sales in zip(stock_row,sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
 
+
+def main():
+    """
+    Run all programme functions
+    """ 
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
+
+print("Welcome to Love Sandwiches Data Automation")
+main()
